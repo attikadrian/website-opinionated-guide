ba69d9930ecb65d103f8d8f745f77cf122ee150a
Sara Vieira <hey@iamsaravieira.com>
2020-08-12 19:01:56 +0200

    fix head tag

--- pages/_document.js
+++ pages/_document.js
@@ -9,82 +9,84 @@ class MyDocument extends Document {
   render() {
     return (
       <Html lang="en">
-        <Head />
-        <title>Opinionated Guide to React</title>
-        <link
-          rel="apple-touch-icon"
-          sizes="180x180"
-          href="/apple-touch-icon.png"
-        />
-        <link
-          rel="icon"
-          type="image/png"
-          sizes="32x32"
-          href="/favicon-32x32.png"
-        />
-        <link
-          rel="icon"
-          type="image/png"
-          sizes="16x16"
-          href="/favicon-16x16.png"
-        />
-        <meta name="msapplication-TileColor" content="#100613" />
-        <meta name="theme-color" content="#100613" />
-        <link
-          href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700&display=swap"
-          rel="stylesheet"
-        />
-        <script src="https://cdn.paddle.com/paddle/paddle.js"></script>
-        <script
-          async
-          data-uid="c358896839"
-          src="https://colossal-artist-8004.ck.page/c358896839/index.js"
-        ></script>
-        <script async defer src="https://cdn.coollabs.io/cool.js"></script>
-        <meta
-          name="description"
-          content="This book is my personal map of the tips, power-ups and
+        <Head>
+          <title>Opinionated Guide to React</title>
+          <link
+            rel="apple-touch-icon"
+            sizes="180x180"
+            href="/apple-touch-icon.png"
+          />
+          <link
+            rel="icon"
+            type="image/png"
+            sizes="32x32"
+            href="/favicon-32x32.png"
+          />
+          <link
+            rel="icon"
+            type="image/png"
+            sizes="16x16"
+            href="/favicon-16x16.png"
+          />
+          <meta name="msapplication-TileColor" content="#100613" />
+          <meta name="theme-color" content="#100613" />
+          <link
+            href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700&display=swap"
+            rel="stylesheet"
+          />
+          <script src="https://cdn.paddle.com/paddle/paddle.js"></script>
+          <script
+            async
+            data-uid="c358896839"
+            src="https://colossal-artist-8004.ck.page/c358896839/index.js"
+          ></script>
+          <script async defer src="https://cdn.coollabs.io/cool.js"></script>
+          <meta
+            name="description"
+            content="This book is my personal map of the tips, power-ups and
           not-so-obvious solutions to common questions and problems that
           I've come across during 4 year of React development."
-        />
-        <meta charSet="utf-8" />
-        <meta
-          name="image"
-          content="https://launch.opinionatedreact.com/fb.png"
-        />
-        <meta
-          itemProp="image"
-          content="https://launch.opinionatedreact.com/fb.png"
-        />
-        <meta name="twitter:card" content="summary_large_image" />
-        <meta name="twitter:site" content="@NikkitaFTW" />
-        <meta name="twitter:creator" content="@NikkitaFTW" />
-        <meta name="twitter:title" content="Opinionated Guide to React" />
+          />
+          <meta charSet="utf-8" />
+          <meta name="image" content="https://opinionatedreact.com/fb.png" />
+          <meta
+            itemProp="image"
+            content="https://opinionatedreact.com/fb.png"
+          />
+          <meta name="twitter:card" content="summary_large_image" />
+          <meta name="twitter:site" content="@NikkitaFTW" />
+          <meta name="twitter:creator" content="@NikkitaFTW" />
+          <meta name="twitter:title" content="Opinionated Guide to React" />
 
-        <meta
-          name="twitter:image"
-          content="https://launch.opinionatedreact.com/twitter.png"
-        />
-        <meta
-          name="twitter:description"
-          content="This book is my personal map of the tips, power-ups and
+          <meta
+            name="twitter:image"
+            content="https://opinionatedreact.com/twitter.png"
+          />
+          <meta
+            name="twitter:description"
+            content="This book is my personal map of the tips, power-ups and
           not-so-obvious solutions to common questions and problems that
           I've come across during 4 year of React development."
-        />
-        <meta property="og:title" content="Opinionated Guide to React" />
-        <meta
-          property="og:description"
-          content="This book is my personal map of the tips, power-ups and
+          />
+          <meta property="og:title" content="Opinionated Guide to React" />
+          <meta
+            property="og:description"
+            content="This book is my personal map of the tips, power-ups and
           not-so-obvious solutions to common questions and problems that
           I've come across during 4 year of React development."
-        />
-        <meta
-          property="og:image"
-          content="https://launch.opinionatedreact.com/fb.png"
-        />
-        <meta property="og:url" content="https://opinionatedreact.com/" />
-        <meta property="og:site_name" content="Opinionated Guide to React" />
-        <meta property="og:type" content="website"></meta>
+          />
+          <meta
+            property="og:image"
+            content="https://opinionatedreact.com/fb.png"
+          />
+          <meta
+            property="og:image:secure_url"
+            content="https://opinionatedreact.com/fb.png"
+          />
+          <meta property="og:url" content="https://opinionatedreact.com/" />
+          <meta property="og:site_name" content="Opinionated Guide to React" />
+          <meta property="og:type" content="website"></meta>
+        </Head>
         <body>
           <Main />
           <NextScript />
